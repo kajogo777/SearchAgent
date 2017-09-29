2c0c7ec26a323a6c992d9b99e20cf96ba501dff3
Rania Wael <rania.waelsameh@yahoo.com>
2017-09-29 15:46:40 +0200

    Create GeneratingGrid.py

---
+++ GeneratingGrid.py
@@ -0,0 +1,46 @@
+from random import *
+
+def GenGrid():
+
+	m = randint(1,10)
+	n = randint(1,10)
+	robotPos = (randint(0, m-1), randint(0, n-1))
+	teleportalPos = (randint(0, m-1), randint(0, n-1))
+	
+	unmovableNum = randint(1,max(m,n))
+	unmovablesPos = []
+
+	for i in range(1, unmovableNum):
+		currUnmovable = (randint(0,m-1), randint(0,n-1))
+		while ((currUnmovable in unmovablesPos) or (currUnmovable == teleportalPos)):
+			currUnmovable = (randint(0,m-1), randint(0,n-1))
+		unmovablesPos.append(currUnmovable)
+
+	rocksNum = randint(1, max(m,n))
+	rocksPos = []
+	pressurePos = []
+
+	for j in range(1, rocksNum):
+		currRock = (randint(0,m-1), randint(0,n-1))
+		currPressure = (randint(0,m-1), randint(0,n-1))
+
+		while ((currPressure in unmovablesPos) or (currPressure == teleportalPos)):
+			currPressure = (randint(0,m-1), randint(0,n-1))
+		
+		pressurePos.append(currPressure) 
+
+		while ((currRock in unmovablesPos) or (currRock in pressurePos) or (currRock == teleportalPos) or (currRock == robotPos)):
+			currRock = (randint(0,m-1), randint(0,n-1))
+		
+		rocksPos.append(currRock) 
+
+	print("m = ",m)
+	print("n = ", n)
+	print("The robot is at ", robotPos)
+	print("Teleportal is at ", teleportalPos)
+	print("There is unmovable objects at ", unmovablesPos)
+	print("The rocks' positions are ", rocksPos)
+	print("And the pressure pads' positions are ", pressurePos)
+	return;
+
+GenGrid()
